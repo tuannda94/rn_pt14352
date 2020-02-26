0674fb1f52b560d977a7e0706e8a68e277a702dd
minhhai0504 <57515153+minhhai0504@users.noreply.github.com>
2020-02-26 08:32:29 +0700

    haidmph07500
    haidmph07500

--- index.js
+++ index.js
@@ -1,74 +1,158 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-import Expo from "expo";
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Image,
+  Switch,
+  Modal,
+  Button,
+  TextInput,
+  Picker,
+  ScrollView
+} from 'react-native';
+import InfoText from './info-text';
+import SubjectItem from './subject-item';
 
-class App extends React.Component {
-  const content = "FPT POLY";
-
-  const gptb1 = (a, b) => {
-    if (a == 0 && b == 0) {
-      return "vo so nghiem";
-    }
-
-    if (a == 0 && b != 0) {
-      return "vo nghiem";
-    }
-
-    if (a != 0 && b != 0) {
-      return -b / a;
-    }
-
-    if (a != 0 && b == 0) {
-      return -b / a;
-    }
+export default function Profile() {
+  const userProfile = {
+    info: {
+      avatar: 'https://iap.poly.edu.vn/user/ph/PH09025.jpg',
+      name: 'Nguyen Van A',
+      email: 'a@gmail.com',
+      address: 'HN',
+      phone: '0123456789',
+      active: true
+    },
+    subjects: [
+      {
+        name: 'React Native',
+        identity: 'MOB306',
+        className: 'PT14352',
+      },
+      {
+        name: 'React Native',
+        identity: 'MOB307',
+        className: 'PT14352',
+      },
+      {
+        name: 'React Native',
+        identity: 'MOB308',
+        className: 'PT14352',
+      }
+    ]
   };
+  // Khai bao state + ham thay doi gia tri cua no
+  const [showInfo, setShowInfo] = useState(true);
+  // Chuyen viec su dung userprofile sang state, de khi user thay doi gia tri se render lai man hinh
+  const [user, setUser] = useState(userProfile);
+  // Tao state kiem soat viec hien thi cua modal add subject
+  const [showModal, setShowModal] = useState(false);
 
-  const valueGpt = gptb1(3, 4); // c1
+  // Khai bao ham thuc hien cong viec xoa
+  const handleDeleteSubject = (identity) => {
+    // su dung let de co the gan gia tri moi
+    let newSubjectList = user.subjects;
+    // filter ((item) => tra ve dieu kien ma item do se duoc xu ly)
+    // sau khi filter xong (chay het vong lap voi dieu kien dua ra) -> tra ve mang moi co cac item thoa man dk
+
+    newSubjectList = newSubjectList.filter((subject) => subject.identity != identity);
+    userProfile.subjects = newSubjectList;
+    console.log(newSubjectList);
+
+    setUser(userProfile);
+  }
+
+  const [nameName, setName] = useState("");
+
+  const [idee, setIde] = useState("");
+
+  const [nameClass, setNameClass] = useState("");
+
+  const addItem = () => {
+    let newSubjectList = user.subjects;
+
+    const subject = {
+      name: nameName.toString(),
+      identity: idee.toString(),
+      className: nameClass.toString(),
+    };
+    newSubjectList = newSubjectList.push(subject);
+    console.log(idee);
+
+  }
 
   return (
-    // <View style={myStyle.app}>
-    //   <Text style={myStyle.text}>{content}</Text>
-    //   <Text>{valueGpt}</Text>
-    //   <Text>{gptb1(5, 0)}</Text>
-    //   <Text>hello poly pt14352-MOB</Text>
-    // </View>
-    <ScrollView>
-        <Text>123123123</Text>
-      <View style={{ flex: 1, alignItems: "center", marginTop: 200 }}>
-        <Text
-          style={{ color: "red", fontSize: 60 }}
-        >{`Truong ten la: ${content}`}</Text>
-        <Image source={require("./PH11674.jpg")} />
-        <Image
-          source={{ uri: "https://iap.poly.edu.vn/logo.png" }}
-          style={myStyle.image}
+    <View style={style.profileContainer}>
+      <View style={style.avatar}>
+        <Image style={style.image} source={{ uri: user.info.avatar }} />
+        <Switch
+          value={showInfo}
+          onValueChange={() => setShowInfo(!showInfo)}
         />
       </View>
-    </ScrollView>
+      <Text>--------------</Text>
+      <View>
+        {showInfo ? <InfoText data={user.info} /> : null}
+        <Text>--------------</Text>
+        <Button
+          title="Add subject"
+          onPress={() => {
+            setShowModal(true);
+          }}
+        />
+        <FlatList
+          data={user.subjects} // mảng nhận vào để hiển thị danh sách
+          renderItem={({ item }) => (
+            <SubjectItem item={item} handleDelete={handleDeleteSubject} />
+          )} // item ứng với {name: '', identity: '', className: ''}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+      <Modal visible={showModal}>
+        <ScrollView>
+
+
+          <View>
+            <Text>Modal Add Subject</Text>
+            <Text>Name</Text>
+            <TextInput onChangeText={(valueName) => setName(valueName)} />
+
+            <Text>Identity</Text>
+            <TextInput onChangeText={(valueIde) => setIde(valueIde)} />
+
+            <Text>Select Class Name</Text>
+            <Picker selectedValue="PT1111" onValueChange={(valueNamClass) => setNameClass(valueNamClass)} >
+              <Picker.Item value="PT1111" label="PT1111" />
+              <Picker.Item value="PT1112" label="PT1112" />
+              <Picker.Item value="PT1113" label="PT1113" />
+
+            </Picker>
+            <Button
+              title="Cancle"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            />
+            <Button
+              title="ok"
+              onPress={() => { addItem(), setShowModal(false) }}
+
+            />
+          </View>
+        </ScrollView>
+      </Modal>
+    </View>
   );
 }
 
-const myStyle = StyleSheet.create({
-  app: {
-    backgroundColor: "#ccc",
-    fontSize: 30
-  },
-  text: {
-    color: "#fff"
-  },
+const style = StyleSheet.create({
+  profileContainer: {},
+  avatar: {},
   image: {
-    width: 300,
-    height: 400
+    width: 200,
+    height: 200,
+    borderRadius: 200
   }
 });
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
-
-export default Expo.registerRootComponent(App);
