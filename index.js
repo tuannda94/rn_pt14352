a90b1b4566f239848a282ba247791950e706b1c5
tuanlabophp <tuan.nguyenducanh@gmail.com>
2020-03-07 11:28:19 +0700

    API fetch delete

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, FlatList, Image, Switch} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Image, Switch, Button} from 'react-native';
 import {registerRootComponent} from 'expo'; // higherOrder component
 
 function App() {
+    const [subjects, setSubjects] = useState([]);
+    const [showList, setShowList] = useState(true);
+    const [showLoading, setShowLoading] = useState(false);
+
     const API = 'https://5e5a60986a71ea0014e61d88.mockapi.io/api/subjects';
     // Dinh nghia ham xu ly cong viec call API
     const fetchSubjects = () => {
@@ -19,39 +23,64 @@ function App() {
         // tham so 1: la 1 arrow function chua cac xu ly lien quan den API
         // tham so 2: la 1 mang, chua cac item la cac bien co su thay doi,
             // neu thay doi thi moi goi tiep cai ham o tham so 1
-        () => {fetchSubjects();}
-        , [showList]
+        () => {
+            fetchSubjects();
+        }
+        , []
         // Neu tham so thu 2 la array, khong co item nao thi se chi chay 1 lan
         // Neu co thi se kiem tra su thay doi va neu thay doi thi goi lai arrow function
         // Hien tai khi state showList thay doi thi arrow function o tren chua call lai du da render lai roi
         // Gia su neu phan trang va click thi state luu page se thay doi nhung api lay du lieu trang moi van chua goi
     )
+    // console.log(subjects, 123123, showList);
 
-    console.log(subjects);
+    const deleteSubject = (id) => {
+        const newSubject = subjects.filter(item => item.id != id);
+        setSubjects(newSubject);
+    }
 
-    const [subjects, setSubjects] = useState([]);
-    const [showList, setShowList] = useState(true);
+    const handleDelete = (id) => {
+        setShowLoading(true);
+        deleteSubject(id);
+
+        fetch(
+            `${API}/${id}`,
+            {method: 'DELETE'}
+        ).then(() => {
+            setShowLoading(false);
+        })
+        .catch((error) => console.log(error));
+    }
+
+    // console.log(subjects);
 
     return (
-        <View style={styles.container}>
-            <Text>List subject</Text>
-            <Switch value={showList} onValueChange={() => setShowList(!showList)} />
-            {showList ?
-            <FlatList
-                data={subjects}
-                renderItem={({item}) =>
-                <View>
-                    <Text>{item.id}</Text>
-                    <Text>{item.identity}</Text>
-                    <Text>{item.name}</Text>
-                    <Text>{item.className}</Text>
-                    <Image style={styles.logo} source={{uri: item.logo}} />
-                </View>
-            }
-            />
-            : null}
-        </View>
-    )
+      <View style={styles.container}>
+        <Text>List subject</Text>
+        <Switch value={showList} onValueChange={() => setShowList(!showList)} />
+        {
+            showLoading
+                ? <Text>LOADING...</Text>
+                : null
+        }
+        {showList ? (
+          <FlatList
+            data={subjects}
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item.id}</Text>
+                <Text>{item.identity}</Text>
+                <Text>{item.name}</Text>
+                <Text>{item.className}</Text>
+                <Image style={styles.logo} source={{ uri: item.logo }} />
+                <Button title="DELETE" onPress={() => handleDelete(item.id)} />
+              </View>
+            )}
+            keyExtractor={(item, index) => item.id}
+          />
+        ) : null}
+      </View>
+    );
 };
 
 const styles = StyleSheet.create({
